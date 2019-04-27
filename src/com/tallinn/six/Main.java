3e123fd1a6e095771a3909011ca1eefa5b040623
Anneli Rasu <annelirasu@gmail.com>
2019-04-27 15:14:30 +0300

    uus projekt

--- src/com/tallinn/six/Main.java
+++ src/com/tallinn/six/Main.java
@@ -3,6 +3,6 @@ package com.tallinn.six;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World and once again! Hello Branch. Hello Master again");
+        System.out.println("Hello World and once again! Hello Branch. Hello Master again.Kloon");
     }
 }
